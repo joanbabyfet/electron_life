24d8f0f9afa4b0b204cf08c22ce0a48b6ed6f9eb
alan <crwu0206@gmail.com>
2024-05-05 11:56:02 +0800

    fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { app, BrowserWindow } = require('electron')
 //引入配置
 require('dotenv').config()
 
-const env = process.env.NODE_ENV //当前环境
+const is_dev = process.env.NODE_ENV === 'development' //当前环境
 //屏蔽安全告警
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 //忽略ssl证书检测
@@ -14,8 +14,8 @@ app.on('ready', () => {
   //创建一个窗口
   const mainWindow = new BrowserWindow({
     //fullscreen: true,
-    width: 600,
-    height: 400,
+    width: 800,
+    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true, //设置该窗口js能访问node环境
@@ -24,13 +24,16 @@ app.on('ready', () => {
   })
   //窗口加载html文件
   //mainWindow.loadFile('index.html')
-  mainWindow.loadURL(
-    env == "development" ? 'http://localhost:3002' : 
-    `file://${path.join(__dirname, '../dist/index.html')}`
-  )
-  if (env == "development") {
+  mainWindow.loadURL('http://localhost:3002')
+  if (is_dev) {
     mainWindow.webContents.openDevTools()
   } else {
     mainWindow.removeMenu()
   }
+})
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此，通常对程序和它们在
+// 任务栏上的图标来说，应当保持活跃状态，直到用户使用 Cmd + Q 退出
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
